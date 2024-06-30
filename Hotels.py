c4b820215698e5f2b24a68c769b5b43ee98aed9b
CharisMaragkakis <maragkakis_xarhs@yahoo.gr>
2024-07-01 01:32:03 +0300

    lists

--- Hotels.py
+++ Hotels.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 from joblib import load
 
+
+df = pd.read_csv('hotelapp.csv')
+
 # Load the trained models
 pipeline_revenue = load('model_pipeline_revenue.joblib')
 pipeline_trips = load('model_pipeline_trips.joblib')
@@ -11,3 +14,9 @@ revenue_per_city_data = pd.read_excel('revenue_per_city.xlsx')
 
 # Streamlit app layout
 st.title('Hotels Prediction Tool')
+
+# Generate lists for dropdown options
+regions = sorted(df['region'].unique().tolist())
+countries = sorted(df['country'].unique().tolist())
+city = sorted(df['city'].unique().tolist())
+partner_types = sorted(df['category'].unique().tolist())
